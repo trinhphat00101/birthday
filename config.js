a4e07400e0d8796052d927fade7b32d116850a22
Trinh Phat <trinhphat001@gmail.com>
2024-11-04 16:33:50 -0500

    Fix language

--- config.js
+++ config.js
@@ -3,17 +3,12 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
-        "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日了哦",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
-        "要把我家可爱猪猪喂饱饱",
-        "然后抱走",
-        "YAMI~~",
+        "Chúc Mừng sinh nhật em iu",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "Tuổi mới hay ăn chóng lớn",  // 同上...
+        "Dành nhiều thời gian nghỉ ngơi cho bản thân",
+        "Bồi bổ bồi dưỡng sức khỏe",
+        "Iu em nhìu",
+        "Love youuuu",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -30,13 +25,13 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        turn_on: "Bấm vào đây nè",
+        play: "Bấm tiếp nè",
+        bannar_coming: "Bấm tiếp đi",
+        balloons_flying: "Còn nữa",
+        cake_fadein: "Còn nữa nè",
+        light_candle: "Bấm tiếp thui",
+        wish_message: "Ước một điều",
         story: "A MESSAGE FOR YOU",
     }
 };
